cb2043a4ea24cae403e58649a726db5b6c116b8b
Suki <suki@missallsunday.com>
2013-10-20 13:09:44 -0500

    Better, shorter name
    Signed-off-by: Suki <suki@missallsunday.com>

--- Ohara.php
+++ Ohara.php
@@ -3,7 +3,7 @@
 /**
  * @package Ohara helper class mod
  * @version 1.0
- * @author Jessica González <missallsunday@simplemachines.org>
+ * @author Jessica González <suki@missallsunday.com>
  * @copyright Copyright (c) 2013, Jessica González
  * @license http://www.mozilla.org/MPL/MPL-1.1.html
  */
@@ -29,7 +29,7 @@
  * the Initial Developer. All Rights Reserved.
  *
  * Contributor(s):
- * Jessica González <missallsunday@simplemachines.org>
+ * Jessica González <suki@missallsunday.com>
  */
 
 class Ohara
@@ -99,9 +99,9 @@ class Ohara
 	/**
 	 * Let's try the singleton method
 	 *
-	 * @return void
+	 * @return object
 	 */
-	public static function getInstance()
+	public static function run()
 	{
 		if (!isset(static::$className))
 			trigger_error('<strong>protected static $className = __CLASS__;</strong> must be contained in child class', E_USER_ERROR);
