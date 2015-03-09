12e7b28b0c95e04c2f6480b327f370206cab87dd
Suki <suki@missallsunday.com>
2015-03-09 11:22:44 -0600

    Doc blocks
    Signed-off-by: Suki <suki@missallsunday.com>

--- src/Suki/Ohara.php
+++ src/Suki/Ohara.php
@@ -10,18 +10,57 @@
 
 namespace Suki;
 
+/**
+ * Helper class for SMF modifications
+ * @package Ohara helper class
+ * @subpackage classes
+ */
 class Ohara
 {
+	/**
+	 * The main identifier for the class extending Ohara, needs to be re-defined by each extending class
+	 * @access public
+	 * @var string
+	 */
 	public $name = '';
+
+	/**
+	 * Text array for holding your own text strings
+	 * @access protected
+	 * @var array
+	 */
 	protected $_text = array();
+
+	/**
+	 * An array holding up all instances extending Ohara
+	 * @static
+	 * @access protected
+	 * @var array
+	 */
 	protected static $_registry = array();
+
+	/**
+	 * Holds any sanitized data from $_REQUEST
+	 * @access protected
+	 * @var array
+	 */
 	protected $_request = array();
 
+	/**
+	 * Getter for {@link $name} property.
+
+	 * @return string
+	 */
 	public function getName()
 	{
 		return $this->name;
 	}
 
+	/**
+	 * Registers your function on {@link $_registry} and sets many properties replacing SMF's global vars
+	 * @access public
+	 * @return void
+	 */
 	public function setRegistry()
 	{
 		global $sourcedir, $scripturl;
@@ -36,11 +75,23 @@ class Ohara
 		static::$_registry[$this->name] = $this;
 	}
 
+	/**
+	 * Getter for {@link $_registry} property.
+	 * @access public
+	 * @param string $instance The name of the instance you want to retrieve, leave empty to retrieve the entire array
+	 * @return string|array|bool
+	 */
 	public function getRegistry($instance = '')
 	{
 		return $instance ? static::$_registry[$instance] : static::$_registry;
 	}
 
+	/**
+	 * Getter for {@link $_text} property.
+	 * @access public
+	 * @param string $var The name of the $txt key you want to retrieve
+	 * @return bool|string
+	 */
 	public function text($var)
 	{
 		global $txt;
@@ -55,6 +106,13 @@ class Ohara
 		return $this->_text[$var];
 	}
 
+	/**
+	 * Loads the extending class language file and sets a new key in {@link $_text}
+	 * Ohara automatically adds the value of {@link $name} plus and underscore to match the exact $txt key when fetching the var
+	 * @access protected
+	 * @param string $var The name of the $txt key you want to retrieve
+	 * @return bool|string
+	 */
 	protected function setText($var)
 	{
 		global $txt;
@@ -73,11 +131,23 @@ class Ohara
 			$this->_text[$var] = false;
 	}
 
+	/**
+	 * Getter for {@link $_text}
+	 * @access public
+	 * @return array
+	 */
 	public function getAllText()
 	{
 		return $this->_text;
 	}
 
+	/**
+	 * Checks for a $modSetting key and its state
+	 * returns true if the $modSetting exists and its not empty regardless of what its value is
+	 * @param string $var The name of the $modSetting key you want to retrieve
+	 * @access public
+	 * @return boolean
+	 */
 	public function enable($var)
 	{
 		global $modSettings;
@@ -92,6 +162,13 @@ class Ohara
 			return false;
 	}
 
+	/**
+	 * Returns the actual value of the selected $modSetting
+	 * uses Ohara::enable() to determinate if the var exists
+	 * @param string $var The name of the $modSetting key you want to retrieve
+	 * @access public
+	 * @return mixed|boolean
+	 */
 	public function setting($var)
 	{
 		global $modSettings;
@@ -107,6 +184,13 @@ class Ohara
 			return false;
 	}
 
+	/**
+	 * Returns the actual value of a generic $modSetting var
+	 * useful to check external $modSettings vars
+	 * @param string $var The name of the $modSetting key you want to retrieve
+	 * @access public
+	 * @return mixed|boolean
+	 */
 	public function modSetting($var)
 	{
 		global $modSettings;
@@ -125,6 +209,13 @@ class Ohara
 			return false;
 	}
 
+	/**
+	 * Sets {@link $_request} with the value of the requested superglobal var
+	 * can be called directly but its also called by Ohara::data()
+	 * @param string $type request, post or get. The name of the superblogal you want to fetch, defaults to request
+	 * @access public
+	 * @return void
+	 */
 	public function setData($type = 'request')
 	{
 		$types = array('request' => $_REQUEST, 'get' => $_GET, 'post' => $_POST);
@@ -134,6 +225,13 @@ class Ohara
 		unset($types);
 	}
 
+	/**
+	 * Sanitizes and returns the requested value.
+	 * calls Ohara::sanitize() to properly clean up
+	 * @param string $var the superglobal's key name you want to retrieve
+	 * @access public
+	 * @return mixed
+	 */
 	public function data($var)
 	{
 		// Forgot something?
@@ -143,6 +241,13 @@ class Ohara
 		return $this->validate($var) ? $this->sanitize($this->_request[$var]) : false;
 	}
 
+	/**
+	 * Checks the var against {@link $_request} to know if it exists and its defined or not.
+	 * calls Ohara::setData() in case {@link $_request} is empty by the time this method its called
+	 * @param string $var the superglobal's key name you want to check
+	 * @access public
+	 * @return boolean
+	 */
 	public function validate($var)
 	{
 		// $var should always be a string, it should be the name of the var you want to validate, not the actual var!
@@ -156,6 +261,13 @@ class Ohara
 		return (isset($this->_request[$var]));
 	}
 
+	/**
+	 * Sanitizes a var. Recursive.
+	 * Treats any var as a string and cast it as an integer if necessary.
+	 * @param mixed $var The var you want to sanitize
+	 * @access public
+	 * @return mixed
+	 */
 	public function sanitize($var)
 	{
 		if (is_array($var))
@@ -180,6 +292,14 @@ class Ohara
 		return $var;
 	}
 
+	/**
+	 * Sets a temp var in $_SESSION.
+	 * Ohara automatically creates a new key in $_SESSION using {@link $name}
+	 * @param string $key The unique identifier for your message
+	 * @param mixed $message ideally to store a message but can be used to store any type of variable
+	 * @access public
+	 * @return void|boolean
+	 */
 	public function setUpdate($key, $message)
 	{
 		// Define an update key for this class.
@@ -195,6 +315,13 @@ class Ohara
 			$_SESSION[$this->name]['update'][$key] = $message;
 	}
 
+	/**
+	 * Gets the var previously added by Ohara::setUpdate()
+	 * calls Ohara::cleanUpdate() to delete the entry from $_SESSION
+	 * @param string $key The unique identifier for your message
+	 * @access public
+	 * @return mixed
+	 */
 	public function getUpdate($key)
 	{
 		if (empty($key))
@@ -208,6 +335,12 @@ class Ohara
 		return $update;
 	}
 
+	/**
+	 * Gets all vars associated with the the extending class using {@link $name}
+	 * calls Ohara::cleanUpdate() to delete the entry from $_SESSION
+	 * @access public
+	 * @return mixed|boolean
+	 */
 	public function getAllUpdates()
 	{
 		$update =  !empty($_SESSION[$this->name]['update']) ? $_SESSION[$this->name]['update'] : false;
@@ -219,6 +352,12 @@ class Ohara
 		return $update;
 	}
 
+	/**
+	 * Deletes the key form $_SESSION
+	 * automatically called by any getter after retrieving the needed message.
+	 * @param string $key The unique identifier for your message
+	 * @access public
+	 */
 	public function cleanUpdate($key)
 	{
 		if (empty($key))
