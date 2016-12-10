63413f8a67d1f37abc6b5fc4f054ff479b8dcb5b
Jessica González <suki@missallsunday.com>
2016-12-09 21:16:20 -0600

    utf8 without BOM encoding
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- src/Suki/Tools.php
+++ src/Suki/Tools.php
@@ -1,10 +1,10 @@
-<?php
+Ôªø<?php
 
 /**
  * @package Ohara helper class
  * @version 1.1
- * @author Jessica Gonz·lez <suki@missallsunday.com>
- * @copyright Copyright (c) 2016, Jessica Gonz·lez
+ * @author Jessica Gonz√°lez <suki@missallsunday.com>
+ * @copyright Copyright (c) 2016, Jessica Gonz√°lez
  * @license http://www.mozilla.org/MPL/2.0/
  */
 
