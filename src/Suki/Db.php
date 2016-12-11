1c3cb014d902e42ae542efde2744be771e3003c4
Jessica González <suki@missallsunday.com>
2016-12-11 17:43:32 -0600

    Its called CRUD right? ...or so I heard
    Signed-off-by: Jessica González <suki@missallsunday.com>

--- src/Suki/Db.php
+++ src/Suki/Db.php
@@ -39,8 +39,48 @@ class Db
 	}
 
 	/**
+	 * Create s anew entry on x table.
+	 * Uses {@link $_schema}
+	 * Uses {@link $_tableKeys}
+	 * @access public
+	 * @param array $data An array of data to be inserted,  array('column' => 'value')
+	 * @param string $table The table name, if left empty and $this->_schema is defined, it will use the first table on it.
+	 * @return mixed The new inserted ID.
+	 */
+	public function create($data, $table = '')
+	{
+		global $smcFunc;
+
+		// What table should be updated?  defaults to $this->_schema[0] if no table was specified.
+		$table = isset($this->_schema[$table]) ? $table : (isset($this->_schema[0]) ? key($this->_schema) : '');
+
+		// We need to make sure you defined this table's columns.
+		if (empty($this->_schema[$table]))
+			return false;
+
+		// Now check the data, perhaps I should check that the columns does match the columns declared for this table but i'm lazy...
+		if (empty($data))
+			return false;
+
+		// Insert!
+		$smcFunc['db_insert']('replace', '{db_prefix}' . ($table) .
+			'', $this->_schema[$table], $data, $this->_app->_tableKeys);
+
+		// Get the newly created ID
+		$newID = $smcFunc['db_insert_id']('{db_prefix}' . ($table), key($this->_schema[$table]));
+
+		// Return the newly inserted ID.
+		return $newID;
+	}
+
+	public function read()
+	{
+
+	}
+
+	/**
 	 * Updates data in x table.
-	 * Uses {@link $_dbSchema}
+	 * Uses {@link $_schema}
 	 * @access public
 	 * @param array $ids An array of ids to check againts, can also use an int.
 	 * @param array $params An array of params (ohrly???) $column => $value pair.
@@ -88,32 +128,6 @@ class Db
 				'ids' => $ids,
 			)
 		);
-	}
-
-	public function create($data, $table = '')
-	{
-		global $smcFunc;
-
-		// What table should be updated?  defaults to $this->_schema[0] if no table was specified.
-		$table = isset($this->_schema[$table]) ? $table : (isset($this->_schema[0]) ? key($this->_schema) : '');
-
-		// We need to make sure you defined this table's columns.
-		if (empty($this->_schema[$table]))
-			return false;
-
-		// Now check the data, perhaps I should check that the columns does match the columns declared for this table but i'm lazy...
-		if (empty($data))
-			return false;
-
-		// Insert!
-		$smcFunc['db_insert']('replace', '{db_prefix}' . ($table) .
-			'', $this->_schema[$table], $data, $this->_app->_tableKeys);
-
-		// Get the newly created ID
-		$newID = $smcFunc['db_insert_id']('{db_prefix}' . ($table), key($this->_schema[$table]));
-
-		// Return the newly inserted ID.
-		return $newID;
 	}
 
 	public function delete($value, $table = '', $column = '')
